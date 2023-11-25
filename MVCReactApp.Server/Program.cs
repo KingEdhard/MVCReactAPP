8529901d940646c84f7fe880092e1145a1361fc3
KingEdhard <eduardoleon2011.el@gmail.com>
2023-11-25 15:21:31 -0600

    Agregar archivos de proyecto.

---
+++ MVCReactApp.Server/Program.cs
@@ -0,0 +1,25 @@
+using MVCReactApp.Server.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ReactMvcContext>();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
+
+app.Run();
